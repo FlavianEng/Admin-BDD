f2ae0acd9e743a70dd84bfd4364db8a45fc65a46
flavian.eng <engevinflavian@gmail.com>
2020-12-07 15:03:55 +0100

    TP3

---
+++ Database-admin/Tp3/insertion.sql
@@ -0,0 +1,5 @@
+INSERT INTO teams.games  VALUES (NOW(), false, "observation");
+INSERT INTO teams.games  VALUES (NOW(), true, "observation");
+INSERT INTO teams.games  VALUES (NOW(), false, "observation");
+INSERT INTO teams.games  VALUES (NOW(), false, "observation");
+INSERT INTO teams.games  VALUES (NOW(), true, "observation");
